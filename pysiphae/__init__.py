f6c871b4c237e18dd35931125d1f40677fe79c0f
Izhar Firdaus <kagesenshi.87@gmail.com>
2015-12-13 15:09:38 +0800

    use simplified configuration

--- pysiphae/__init__.py
+++ pysiphae/__init__.py
@@ -18,42 +18,12 @@ def main(global_config, **settings):
     config.load_zcml('pysiphae:configure.zcml')
     config.include('pyramid_chameleon')
     config.add_static_view('++static++', 'static', cache_max_age=3600)
-
-    cp = ConfigParser()
-    cp.readfp(open(global_config['__file__']))
-
-    for route in _get_routes(cp):
-        config.add_route(route['name'], route['pattern'])
+    config.add_route('home','/')
     config.scan()
 
-    plugins = _get_plugins(cp)
+    plugins = settings.get('pysiphae.plugins', '').strip().split()
     for plugin in plugins:
         config.scan(plugin)
         config.load_zcml(plugin + ':configure.zcml')
 
     return config.make_wsgi_app()
-
-def _get_plugins(cp):
-    if not cp.has_section('pysiphae'):
-        return []
-    if not cp.has_option('pysiphae','plugins'):
-        return []
-    return cp.get('pysiphae','plugins').strip().split()
-
-def _get_routes(cp):
-    if not cp.has_section('pysiphae'):
-        return []
-    if not cp.has_option('pysiphae','routes'):
-        return []
-    v = cp.get('pysiphae','routes')
-    vv = [x.split() for x in v.strip().split('\n')]
-    return [{'name': x[0], 'pattern': x[1]} for x in vv]
-
-def _get_adapters(cp):
-    if not cp.has_section('pysiphae'):
-        return []
-    if not cp.has_option('pysiphae','routes'):
-        return []
-    v = cp.get('pysiphae','routes')
-    vv = [x.split() for x in v.strip().split('\n')]
-    return [{'name': x[0], 'pattern': x[1]} for x in vv]
