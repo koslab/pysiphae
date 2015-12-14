d56eecc488147a6eceea3cdc179a8349fcd348c0
Izhar Firdaus <kagesenshi.87@gmail.com>
2015-12-14 18:33:48 +0800

    add templer template for generating dash plugin

---
+++ pysiphae/templer/__init__.py
@@ -0,0 +1,6 @@
+from templer.core import BasicNamespace
+
+class DashPlugin(BasicNamespace):
+    _template_dir = 'templates/dash_plugin'
+    summary = 'Skeleton for Pysiphae Dashboard Plugin'
+    category = 'Pysiphae'
